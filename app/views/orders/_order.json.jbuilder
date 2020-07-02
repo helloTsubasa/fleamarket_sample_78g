165ce335f2c65d33ddf7cd1cebbcd20f27852436
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-02 16:26:31 +0900

    order雛形とimage雛形を削除
    一連ファイルを一旦全削除します。

--- app/views/orders/_order.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! order, :id, :family_name, :first_name, :family_name_kana, :first_name_kana, :zip_code, :prefecture, :city, :local, :building_name, :phone_number, :user_id, :created_at, :updated_at
-json.url order_url(order, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :brands
-  resources :images
-  resources :orders
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
