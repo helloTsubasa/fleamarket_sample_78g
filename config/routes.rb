05da5bfead4c23104a83a3a530848242e5a85a62
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-07 16:23:14 +0900

    ルーティングでuserにアクションを追加する方法を修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :brands
   resources :items
-  resources :users, only:[:index]
-  get 'users/card_data'
-  get 'users/card_register'
-  get 'users/index'
-  get 'users/logout'
-  get 'users/mypage'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only:[:index] do
+    collection do
+      get 'card_data'
+      get 'card_register'
+      get 'index'
+      get 'logout'
+      get 'mypage'
+    end
+  end
+
 end
