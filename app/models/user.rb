190883d7798c69b96bb5a7ac18db3899d3752de5
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-07 12:15:14 +0900

    user - cardのアソシエーションを定義
    ・user has_many cards
    ・card belongs_to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   
   has_many :items,  dependent: :destroy
   has_one  :order,  dependent: :destroy
+  has_many :cards,  dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
