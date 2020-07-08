ae09034a5a6a940a529a81fa013056cf715ecced
Kento <46912492+Kentoo21@users.noreply.github.com>
2020-07-08 19:23:47 +0900

    initial commit
    index.html.hamlに追加
    application.scss作成
    reset.scss追加
    items.scss追加
    imagesに画像を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,15 +54,16 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   # DELETE /items/1.json
   def destroy
-    if @item.user == current_user do
-      if @item.destroy do
-      respond_to do |format|
-        format.html { redirect_to items_url, notice: '商品が削除されました' }
-        format.json { head :no_content }
-      end
-      respond_to do |format|
-        format.html { redirect_to items_url, notice: '商品の削除に失敗しました' }
-        format.json { head :no_content }
+    if @item.user == current_user
+      if @item.destroy
+        respond_to do |format|
+          format.html { redirect_to items_url, notice: '商品が削除されました' }
+          format.json { head :no_content }
+        end
+        respond_to do |format|
+          format.html { redirect_to items_url, notice: '商品の削除に失敗しました' }
+          format.json { head :no_content }
+        end
       end
     end
   end
