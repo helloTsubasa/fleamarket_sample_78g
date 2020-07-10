82dc7f672cb849c9a61e06852e1a9c8f178fc726
Ko-M27 <m.kodai9527@gmail.com>
2020-07-10 17:53:27 +0900

    出品ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :brands
-  resources :items
+  resources :items do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+    member do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   
   resources :users, only:[:index, :destroy] do
     collection do
