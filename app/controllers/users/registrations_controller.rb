85037091ab34279c006388500557dff2448b3683
bebetakaaki <bebetakaaki@gmail.com>
2020-07-11 20:57:51 +0900

    saveの分岐処理

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,24 +24,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render :new_order and return
     end
     @user.build_order(@order.attributes)
-    @user.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
+    if @user.save
+      puts "登録に成功しました"
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      puts "登録に失敗しました"
     end
-
+    
     session["devise.regist_data"]["user"].clear
     sign_in(:user, @user)
   end
