522150ad760c83b2d8af2afda92b37cdfd1ba593
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-18 19:47:07 +0900

    gemのrails-i18nをuninstall
    ついでに投稿画像のサイズを100x100から1080x1080に変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_fit: [1080, 1080]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
