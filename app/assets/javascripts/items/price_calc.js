6a22812a8bd4ce05d0e986f4e0fa3930000116e2
Ko-M27 <m.kodai9527@gmail.com>
2020-07-19 17:03:47 +0900

    7/19

--- app/assets/javascripts/items/price_calc.js
+++ app/assets/javascripts/items/price_calc.js
@@ -1,16 +1,18 @@
-$(function(){
-  $('.price_form').on('input', function(){ 
-    var data = $('.price_form').val();
-    var profit = Math.round(data * 0.9) 
-    var fee = (data - profit) 
-    $('.right_bar').html(fee) 
-    $('.right_bar').prepend('¥') 
-    $('.right_bar_2').html(profit)
-    $('.right_bar_2').prepend('¥')
-    $('#price').val(profit) 
-    if(profit == '') { 
-    $('.right_bar_2').html('');
-    $('.right_bar').html('');
-    }
-  })
+$(document).on('turbolinks:load', ()=>{
+  $(function(){
+    $('.price_form').on('input', function(){ 
+      var data = $('.price_form').val();
+      var profit = Math.round(data * 0.9) 
+      var fee = (data - profit) 
+      $('.right_bar').html(fee) 
+      $('.right_bar').prepend('¥') 
+      $('.right_bar_2').html(profit)
+      $('.right_bar_2').prepend('¥')
+      $('#price').val(profit) 
+      if(profit == '') { 
+      $('.right_bar_2').html('');
+      $('.right_bar').html('');
+      }
+    })
+  });
 });
