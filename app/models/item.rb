c593d4be43067b3e2d9b11a645ee0a01ec29f27d
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-07-20 18:53:27 +0900

    メール・パスワードの変更
    メール・パスワードの変更の画面の作成、及び動作確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user_buyer, class_name: 'User', :foreign_key => 'user_buyer_id', optional: true,dependent: :destroy
+  belongs_to :user_buyer, class_name: 'User', :foreign_key => 'user_buyer_id', optional: true
   belongs_to :user_seller, class_name: 'User', :foreign_key => 'user_seller_id' 
   belongs_to :category
   belongs_to :brand, optional: true
