ea3049b5e3bd44cbafb58c8c79d65b3e49b26b59
Ko-M27 <m.kodai9527@gmail.com>
2020-07-21 18:01:03 +0900

    New_image

--- app/assets/javascripts/items/form_imaage.js
+++ app/assets/javascripts/items/form_imaage.js
@@ -1,15 +1,22 @@
 $(document).on('turbolinks:load', ()=>{
   const buildFileField = (index)=>{
     const html = `<div data-index="${index}" class="image_file">
-                    <input class="image_form" type=file
-                    name="item[images_attributes][${index}][image]"
-                    id="item_images_attributes_${index}_image">
-                    <span class="image_remove">削除</span>
+                    <label for="item_images_attributes_${index}_image">
+                      <div class="image_file__field" id="image_file_${index}"></div>
+                      <input class="image_form" type=file
+                      name="item[images_attributes][${index}][image]"
+                      id="item_images_attributes_${index}_image">
+                    </label>
                   </div>`;
     return html;
   }
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<div data-index="${index}" class="image_view">
+                    <div class="image_src">
+                      <img data-index="${index}" src="${url}" width="100px" height="100px">
+                    </div>
+                    <div class="image_remove">削除</div>
+                  </div>`;
     return html;
   }
 
@@ -18,14 +25,14 @@ $(document).on('turbolinks:load', ()=>{
   fileIndex.splice(0, lastIndex);
 
   $('.image_box').on('change', '.image_form', function(e){
-    const targetIndex = $(this).parent().data('index');
-    console.log(targetIndex);
+    const targetIndex = $(this).parent().parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {
       $('#previews').append(buildImg(targetIndex, blobUrl));
+      $('.image_file').css('display', 'none');
       $('.image_box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
@@ -36,7 +43,10 @@ $(document).on('turbolinks:load', ()=>{
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().remove();
+    console.log($(this).parent());
     $(`img[data-index="${targetIndex}"]`).remove();
+    $(`div[data-index="${targetIndex}"]`).remove();
+  
     if ($('.image_form').length == 0) $('.image_box').append(buildFileField(fileIndex[0]));
   });
 });
